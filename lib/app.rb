0cf79d32a6e42f732d11ca00b0453a338fe45d11
benjamin-white <benjaminwhite@live.co.uk>
2016-07-18 18:03:14 +0100

    adds pretty cat

--- lib/app.rb
+++ lib/app.rb
@@ -6,6 +6,12 @@ get '/' do
   "Hello World"
 end
 
+get '/cat' do
+  #'<div style="border:5px dotted red;">
+    '<img src="http://bit.ly/1eze8aE" style="border:5px dotted red;">'
+  #</div>'
+end
+ style="border:5px dotted red;"
 get '/secret' do
   "This is a secret"
 end
